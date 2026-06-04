188f597063eaff0b9b87147f07321df3c4390df4
Al Rodriguez <ProgrammerAl@Outlook.com>
2026-06-03 22:35:58 -0400

    Deleted more snapshot test files

--- confoo-2026/coding-csharp-with-csharp/demo-code/public-interface-generator/src/UnitTests/Snapshots/Properties/PropertiesTests.ComplexProperties_AssertResults#IMyClass.g.verified.cs
+++
@@ -1,8 +0,0 @@
-﻿//HintName: IMyClass.g.cs
-#nullable enable
-namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.UnitTestClasses;
-
-public interface IMyClass
-{
-    ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.UnitTestClasses.NonDemoClass1? ComplexProperty1 { get; set; }
-}
