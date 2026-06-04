8a99b108c708ce7ec4278d90e8fd6f7cb48ef133
Al Rodriguez <ProgrammerAl@Outlook.com>
2026-06-03 22:35:42 -0400

    deleted more snapshot test files

--- confoo-2026/coding-csharp-with-csharp/demo-code/public-interface-generator/src/UnitTests/Snapshots/Methods/MethodsTests.AsyncMethodTrturnsNullableType_AssertHasNullableAnnotation#IMyClass.g.verified.cs
+++
@@ -1,8 +0,0 @@
-﻿//HintName: IMyClass.g.cs
-#nullable enable
-namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.UnitTestClasses;
-
-public interface IMyClass
-{
-    Task<string?> MyMethod(int arg1);
-}
